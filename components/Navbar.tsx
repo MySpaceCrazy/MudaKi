b8b13ffbbfb3af1ea52a66138dd660beb03d384a
Anderson Oliveira <anderson.oliveira.ao844@gmail.com>
2025-10-01 16:44:03 -0300

    logo tamanho

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,8 +10,8 @@ export default function Navbar() {
           <Image
             src="/mudaki-logo.jpg"
             alt="Logo Mudaki"
-            width={60}
-            height={60}
+            width={120}
+            height={120}
             className="rounded-full object-contain"
             priority
           />
